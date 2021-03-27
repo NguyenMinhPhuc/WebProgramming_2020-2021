12b34230e2095f1f5c238d3cfc2cbbbf21b58079
Nguyen Minh Phuc <nmphuc@outlook.com>
2021-03-27 08:27:01 +0700

    them webshopHocTap.sql

---
+++ Project18CT113/Project_20210327_SIUD_ADO/Project_20210327_SIUD_ADO/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Project_20210327_SIUD_ADO.MvcApplication" Language="C#" %>
---
+++ Project18CT113/Project_20210327_SIUD_ADO/Project_20210327_SIUD_ADO/Models/Database.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Project_20210327_SIUD_ADO.Models
+{
+    public class Database
+    {
+        private SqlConnection connect;
+        private SqlCommand command;
+    }
+}
