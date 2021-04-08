fb77ac615abb229b4373c1a20f6e793d4dd35f27
Nguyen Minh Phuc <nmphuc@outlook.com>
2021-04-08 15:10:52 +0700

    cap nhat bai tao 18CT111

---
+++ Project18CT111/Project_20210406_PhanVungAdmin/Project_20210406_PhanVungAdmin/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace Project_20210406_PhanVungAdmin.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+             defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+        }
+    }
+}
---
+++ Project18CT111/Project_20210406_PhanVungAdmin/Project_20210406_PhanVungAdmin/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,92 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_20210406_PhanVungAdmin.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        string err = string.Empty;
+        // GET: Admin/Category
+        public ActionResult Index()
+        {
+            var Categories = new CategoryDb().GetCategories(ref err);
+            return View(Categories);
+        }
+
+        // GET: Admin/Category/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Admin/Category/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Category/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/Category/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/Category/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/Category/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/Category/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
