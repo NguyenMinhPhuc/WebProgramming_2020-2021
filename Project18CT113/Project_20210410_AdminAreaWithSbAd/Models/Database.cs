c1241473dd1e0bd9249f6b3057ce3e4e94df9f93
Nguyen Minh Phuc <nmphuc@outlook.com>
2021-04-09 21:33:07 +0700

    Chuan bi

---
+++ Project18CT113/Project_20210410_AdminAreaWithSbAd/Models/Database.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Models
+{
+
+    //Sinh viên xây dựng lớp database để kết nối đến SQL Server 
+    //1. Khai báo biến của ADO.NET
+    //2. Khởi tạo đối tượng kết nối bằng hàm tạo
+    //3. Viết hàm trả về danh sách 
+    //4. Viết hàm thực hiện Non Query
+    //5. Viết hàm trả vể dataTable
+    public class Database
+    {
+    }
+}
