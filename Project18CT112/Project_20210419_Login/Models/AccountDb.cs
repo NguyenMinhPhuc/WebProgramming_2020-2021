f012bffd795453319b730ac148b9f0e973574525
Nguyen Minh Phuc <nmphuc@outlook.com>
2021-04-19 11:07:25 +0700

    18ct112_2021_04_19

---
+++ Project18CT112/Project_20210419_Login/Models/AccountDb.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class AccountDb
+    {
+        Database data;
+        public AccountDb()
+        {
+            data = new Database();
+        }
+
+        public bool CheckLogin(ref string err,string userName,string passWord)
+        {
+            return (bool)data.MyExecuteScalar(ref err, "SP_Account_Login", CommandType.StoredProcedure,
+                new SqlParameter("@UserName", userName),
+                new SqlParameter("@PassWord", passWord));
+        }
+    }
+}
---
+++ Project18CT112/Project_20210419_Login/Project_20210419_Login/Areas/Admin/Commons/SessionHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_20210419_Login.Areas.Admin.Commons
+{
+    public class SessionHelper
+    {
+        public static void SetSession(UserSession session)
+        {
+            HttpContext.Current.Session["SessionLogin"] = session;
+        }
+
+        public static UserSession GetSession()
+        {
+            return HttpContext.Current.Session["SessionLogin"] as UserSession;
+        }
+    }
+}
